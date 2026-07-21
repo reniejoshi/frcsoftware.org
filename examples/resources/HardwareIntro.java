f2a14c1f3c2bd9b0acc4db7581ddf32572b8e070
Renie Joshi <renie_joshi@yahoo.com>
2026-07-21 14:03:41 -0700

    Update HardwareIntro to commands v3

--- examples/resources/HardwareIntro.java
+++ examples/resources/HardwareIntro.java
@@ -3,25 +3,44 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-public class HardwareIntro {
-    // [intakeSubsystem]
-    public class IntakeSubsystem extends SubsystemBase {
-        private TalonFX motor = new TalonFX(0);
+import org.wpilib.command3.Mechanism;
+import org.wpilib.hardware.imu.OnboardIMU;
+import org.wpilib.hardware.imu.OnboardIMU.MountOrientation;
+import org.wpilib.math.geometry.Rotation2d;
 
-        public void runIntake() {
-            motor.set(0.8);
+public class HardwareIntro {
+    // [intake]
+    public class Intake implements Mechanism {
+        // Placeholder for TalonFX, SparkMax or SparkFlex
+        private final ExampleMotor motor = new ExampleMotor(0, CANBus.systemcore(0));
+
+        public Command runIntake() {
+            return run(coroutine -> {
+                while (true) {
+                    motor.set(0.8);
+                    coroutine.yield();
+                }
+            })
+            .named("Run Intake");
         }
 
-        public void stopIntake() {
-            motor.set(0.0);
+        public Command stopIntake() {
+            return run(coroutine -> {
+                while (true) {
+                    motor.set(0.0);
+                    coroutine.yield();
+                }
+            })
+            .named("Stop Intake");
         }
     }
-    // [/intakeSubsystem]
+    // [/intake]
 
     // [turret]
-    public class Turret extends SubsystemBase {
-        CANcoder encoder = new CANcoder(1, "rio");
-        
+    public class Turret extends Mechanism {
+        // Placeholder for magnetic encoder
+        private final ExampleEncoder encoder = new ExampleEncoder(1, CANBus.systemcore(0));
+
         public Rotation2d getEncoderPosition() {
             double position = encoder.getAbsolutePosition().getValue().in(Rotations) * ENCODER_MECHANISM_RATIO;
             return Rotation2d.fromRotations(position);
@@ -29,13 +48,13 @@ public class HardwareIntro {
     }
     // [/turret]
 
-    // [chassisSubsystem]
-    public class ChassisSubsystem {
-        private final Pigeon2 pigeon = new Pigeon2(2, "rio");
+    // [drivetrain]
+    public class Drivetrain implements Mechanism {
+        private final OnboardIMU imu = new OnboardIMU(MountOrientation.FLAT);
 
         public Rotation2d getHeading() {
-            return Rotation2d.fromDegrees(pigeon.getYaw());
+            return imu.getRotation2d();
         }
     }
-    // [/chassisSubsystem]
+    // [/drivetrain]
 }
